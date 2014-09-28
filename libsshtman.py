8fc51355efe61fdd4dcfa66dc1619cde64d7419c
Alexander Nyrkov <nyrkovalex@gmail.com>
2014-09-29 00:46:35 +0400

    tests added, listener refactored

--- libsshtman.py
+++ libsshtman.py
@@ -1,17 +1,30 @@
-import os, json, time
+import json, time, subprocess, os, logging
 
 FIFO_PATH = '/tmp/sshtman_daemon_in'
 CHECK_INTERVAL = 0.5
 
-def command(fn):
+def command(function):
     def decorator(*args, **kwargs):
         stripped_kwargs = _strip_empty_values(kwargs)
         stripped_args = [a for a in args if a is not None]
-        return fn(*stripped_args, **stripped_kwargs)
+        return function(*stripped_args, **stripped_kwargs)
     return decorator
 
-def _strip_empty_values(d):
-    return dict(((k, v) for k, v in d.items() if v is not None))
+def value_or_error(value, message=None):
+    if value is None:
+        raise ValueError(message)
+    return value
+
+def _strip_empty_values(dictionary):
+    return dict(((k, v) for k, v in dictionary.items() if v is not None))
+
+def _create_fifo(path):
+    if os.path.exists(path):
+        os.unlink(path)
+    os.mkfifo(path)
+
+def create_tunnel(**kwargs):
+    return Tunnel(**kwargs)
 
 
 class Client:
@@ -26,30 +39,28 @@ class Client:
 
     def perform_action(self, action, name, **kwargs):
         self._actions[action](name, **kwargs)
-        
+
     @command
     def open_tunnel(self, name, **kwargs):
-        command = {
+        self.send({
             'name': 'topen',
             'args': {
                 'name': name,
                 'kwargs': kwargs,
             },
-        }
-        self.send(command)
+        })
 
     @command
     def debug(self):
         self.send({
             'name': 'debug',
-            'args': { 'debug': True },
+            'args': {'debug': True},
         })
 
     @command
     def kill_daemon(self):
         self.send({
             'name': 'die',
-            'args': None,
         })
 
     @command
@@ -68,88 +79,120 @@ class Client:
             fifo.write(cmd_text + '\n')
 
 
-class Listener:
+class Daemon:
 
-    def __init__(self, path, manager):
-        self.path = path
-        self.manager = manager
-        self._commands = {
-            'debug': self._debug,
-            'die': self._die,
-            'topen': self._topen,
-            'tclose': self._tclose,
+    def __init__(self):
+        self._manager = TunnelManager(logging)
+        commands = {
+            'debug': self.debug,
+            'die': self.die,
+            'topen': self.topen,
+            'tclose': self.tclose,
         }
+        self._listener = Listener(commands)
+        self._pipe = None
 
-    def listen(self):
-        self._create_fifo()
-        with open(self.path, 'r') as fifo:
-            self._loop(fifo)
-        os.unlink(self.path)
+    def run(self):
+        with open(FIFO_PATH, 'r') as fifo:
+            self._pipe = Pipe(fifo)
+            self._listener.listen(self._pipe)
 
-    def _loop(self, fifo):
+    @command
+    def debug(self, args):
+        print('Debug command called with\n' + str(args))
+
+    @command
+    def die(self):
+        self._pipe.close()
+
+    @command
+    def topen(self, args):
+        self._manager.open(args['name'], **args['kwargs'])
+
+    @command
+    def tclose(self, args):
+        self._manager.close(args['name'])
+
+
+class Pipe:
+
+    def __init__(self, pipe_file):
+        self._pipe_file = pipe_file
         self._running = True
-        while self._running:
-            for line in fifo:
-                self._exec_command(line)
-            time.sleep(CHECK_INTERVAL)
 
-    def _create_fifo(self):
-        if os.path.exists(self.path):
-            os.unlink(self.path)
-        os.mkfifo(self.path)
+    def __iter__(self):
+        return self
 
-    def _exec_command(self, cmd_text):
-        cmd_spec = json.loads(cmd_text)
-        cmd = self._commands[cmd_spec['name']]
-        cmd(cmd_spec['args'])
-
-    @command
-    def _debug(self, args):
-        print('Debug command called with')
-        print(str(args))
-
-    @command
-    def _die(self):
+    def close(self):
         self._running = False
 
-    @command
-    def _topen(self, args):
-        self.manager.open(args['name'], **args['kwargs'])
+    def __next__(self):
+        while self._running:
+            line = self._pipe_file.readline()
+            if line:
+                return json.loads(line)
+            time.sleep(CHECK_INTERVAL)
+        raise StopIteration
 
-    @command
-    def _tclose(self, args):
-        self.manager.close(args['name'])
+class Listener:
 
+    def __init__(self, commands):
+        self._commands = commands
+
+    def listen(self, pipe):
+        for cmd_spec in pipe:
+            self._exec_command(cmd_spec)
+
+    def _exec_command(self, cmd_spec):
+        cmd = self._commands[cmd_spec['name']]
+        if 'args' in cmd_spec:
+            cmd(cmd_spec['args'])
+        else:
+            cmd()
 
 
 class TunnelManager:
 
-    def __init__(self):
+    def __init__(self, logger):
         self._tunnels = {}
+        self._logger = logger
 
     def open(self, name, **targs):
-        t = Tunnel(**targs)
-        t.open()
-        self._tunnels[name] = t
+        try:
+            tunnel = create_tunnel(**targs)
+            tunnel.open()
+            self._tunnels[name] = tunnel
+            self._logger.debug('Tunnel %s opened' % name)
+        except:
+            self._logger.exception('Failed to create tunnel')
 
     def close(self, name):
+        if name not in self._tunnels:
+            self._logger.warning('Trying to close nonexisting tunnel ' + name)
+            return
         self._tunnels[name].close()
+        self._logger.debug('Tunnel %s close' % name)
 
 
 class Tunnel:
 
     def __init__(self, user=None, host=None, remote_port=None, local_port=None,
                  ssh_port=22):
-        self.user = user
-        self.host = host
-        self.remote_port = str(remote_port)
-        self.local_port = str(local_port)
+        self.user = value_or_error(user, 'No user provided')
+        self.host = value_or_error(host, 'No host provided')
+        self.remote_port = str(value_or_error(remote_port,
+                                              'No remote_port provided'))
+        self.local_port = str(value_or_error(local_port,
+                                             'No local_port provided'))
         self.ssh_port = str(ssh_port)
+        self._process = None
 
     def open(self):
         args = self._create_ssh_command()
         self._process = self._open_process(args)
-        print('SSH tunnel opened')
+
+    def close(self):
+        self._process.terminate()
 
     def _create_ssh_command(self):
         return ('ssh', '-N', '-L',
@@ -160,7 +203,3 @@ class Tunnel:
         return subprocess.Popen(args, stdin=subprocess.PIPE,
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.STDOUT)
-
-    def close(self):
-        self._process.terminate()
-        print('SSH tunnel closed')
---
+++ tests.py
@@ -0,0 +1,83 @@
+import unittest
+from unittest.mock import MagicMock, Mock, patch
+from libsshtman import *
+
+
+class ListenerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.cmd = Mock()
+        commands = {'test': self.cmd}
+        self.listener = Listener(commands)
+
+    def test_should_call_target_command(self):
+        self.listener.listen([{'name': 'test'}])
+        self.assertTrue(self.cmd.called)
+
+    def test_should_pass_arguments_to_command(self):
+        self.listener.listen([{'name': 'test', 'args': 'foo'}])
+        self.cmd.assert_called_with('foo')
+
+
+@patch('libsshtman.create_tunnel')
+class TunnelManagerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.tunnel = Mock()
+        self.logger = Mock()
+        self.manager = TunnelManager(self.logger)
+
+    def test_should_create_new_tunnel(self, create_tunnel):
+        self.manager.open('test')
+        self.assertTrue(create_tunnel.called)
+
+    def test_should_add_tunnel_to_inner_dict(self, create_tunnel):
+        create_tunnel.return_value = self.tunnel
+        self.manager.open('test')
+        self.assertTrue(self.tunnel in self.manager._tunnels.values())
+
+    def test_should_not_fall_on_any_error(self, create_tunnel):
+        create_tunnel.side_effect = Exception()
+        self.manager.open('test')
+
+    def test_should_log_error_on_tunnel_creation_failure(self, create_tunnel):
+        create_tunnel.side_effect = ValueError()
+        self.manager.open('test')
+        self.assertTrue(self.logger.exception.called)
+
+    def test_should_log_warning_closing_nonexisting_tunnel(self, create_tunnel):
+        self.manager.open('test')
+        self.manager.close('foo')
+        self.assertTrue(self.logger.warning.called)
+
+
+class TunnelTest(unittest.TestCase):
+
+    def test_should_raise_error_if_no_user_provided(self):
+        self.assertRaises(ValueError, create_tunnel,
+            host='test',
+            remote_port=23,
+            local_port=23
+        )
+
+    def test_should_raise_error_if_no_host_provided(self):
+        self.assertRaises(ValueError, create_tunnel,
+            user='test',
+            remote_port=23,
+            local_port=23
+        )
+
+    def test_should_raise_error_if_no_remote_port_provided(self):
+        self.assertRaises(ValueError, create_tunnel,
+            user='test',
+            host='test',
+            local_port=23
+        )
+
+    def test_should_raise_error_if_no_local_port_provided(self):
+        self.assertRaises(ValueError, create_tunnel,
+            user='test',
+            host='test',
+            remote_port=23
+        )
+
